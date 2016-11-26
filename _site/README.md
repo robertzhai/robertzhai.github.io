4951cdb6ff8da36a63a37f0ef8b999d7f28c6643
zhaishiwei <zhaishiwei@baidu.com>
2016-11-26 10:09:29 +0800

    update this repo

--- _site/README.md
+++
@@ -1,5 +0,0 @@
-#robertzhai.github.io , 我的个人博客 robertzhai.github.io, 基于 jekyll  
-[https://robertzhai.github.io/](https://robertzhai.github.io/)
- 
-
-
--- _site/build_push.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-#gulp
-#rsync -avr build/* .
-JEKYLL_ENV=production jekyll build --incremental
-git add *
-git ci -am 'update this repo'
-echo "start push .."
-expect ~/.account/github.sh
-echo " end push ..."
