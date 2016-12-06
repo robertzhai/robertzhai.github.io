a3659410f1eb705a342495163eb2e3a82149291d
zhaishiwei <zhaishiwei@baidu.com>
2016-12-06 22:58:40 +0800

    update this repo

--- build_push.sh
+++ build_push.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 #gulp
 #rsync -avr build/* .
-JEKYLL_ENV=production jekyll build --incremental
+#JEKYLL_ENV=production jekyll build --incremental
+JEKYLL_ENV=production jekyll build 
 git add *
 git ci -am 'update this repo'
 echo "start push .."
